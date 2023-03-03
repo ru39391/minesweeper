a224479c46c4c92a9d18a3306655bea4f08b3c2c
ru39391 <dev@39391.ru>
2023-03-03 23:11:29 +0300

    feature/настроить получение соседних элементов

--- src/js/Minecell.js
+++ src/js/Minecell.js
@@ -10,8 +10,10 @@ export class Minecell {
     this._cellClassName = cellClassName;
     this._mineClassName = mineClassName;
 
+    this._initCellIndex = null;
     this._cellElemsArr = [];
-    this._cellsArrLength = Math.pow(cellSideLength, 2);
+    this._cellSideLength = cellSideLength;
+    this._cellsArrLength = Math.pow(this._cellSideLength, 2);
 
     this._mineElemsArr = [];
     this._minesArrLength = minesLength;
@@ -23,12 +25,14 @@ export class Minecell {
   _createEl(data) {
     const {tagName, className, parentEl} = data;
     const el = document.createElement(tagName);
+
     el.classList.add(className);
     parentEl.append(el);
+
     return el;
   }
 
-  _filterRandIndexArr(arr) {
+  _removeEqElems(arr) {
     const filtredArr = arr.reduce((acc, item) => {
       if(acc.find(ind => ind === item)) {
         return acc;
@@ -38,7 +42,41 @@ export class Minecell {
 
     console.log(arr.length, filtredArr.length, filtredArr.filter((item, index) => index < this._minesArrLength).length);
 
-    return filtredArr.filter((item, index) => index < this._minesArrLength);
+    /*
+    console.log(array.map((item, index) => {
+    }));
+    */
+
+    return filtredArr;
+  }
+
+  _getSinbings(target) {
+    const positions = {
+      top: target - this._cellSideLength,
+      right: target + 1,
+      bottom: target + this._cellSideLength,
+      left: target - 1
+    };
+    const { top, right, bottom, left } = positions;
+
+    const isSide = {
+      isTopSide: !(top >= 0),
+      isRightSide: !(right % this._cellSideLength),
+      isBottomSide: !(bottom < this._cellsArrLength),
+      isLeftSide: !(target % this._cellSideLength),
+    };
+    const { isTopSide, isRightSide, isBottomSide, isLeftSide } = isSide;
+
+    const siblingsData = {
+      topSiblings: isTopSide ? !isTopSide : [top - 1, top, top + 1],
+      rightSiblings: isRightSide ? !isRightSide : right,
+      bottomSiblings: isBottomSide ? !isBottomSide : [bottom - 1, bottom, bottom + 1],
+      leftSiblings: isLeftSide ? !isLeftSide : left
+    };
+
+    console.log(Object.values(isSide).map((item, index) => {
+      return item ? !item : Object.values(siblingsData)[index];
+    }));
   }
 
   _setRandIndexArr() {
@@ -50,11 +88,21 @@ export class Minecell {
       i++;
     }
 
-    return this._filterRandIndexArr(indexArr);
+    const initCellIndex = indexArr.indexOf(this._initCellIndex);
+    if(initCellIndex >= 0) {
+      indexArr.splice(initCellIndex, 1);
+    }
+
+    console.log(initCellIndex);
+    console.log(this._initCellIndex);
+    return this._removeEqElems(indexArr).filter((item, index) => index < this._minesArrLength);
   }
 
-  _setMineElems() {
+  _setElemsType() {
     this._mineElemsArr = this._setRandIndexArr().map(item => this._cellElemsArr[item]);
+    this._getSinbings(this._initCellIndex);
+
+
     this._mineElemsArr.forEach((item) => {
       item.classList.add(this._mineClassName);
     });
@@ -62,17 +110,20 @@ export class Minecell {
 
   _setInitActive(e) {
     console.log(`1: ${this._isInit}`);
+    const { target } = e;
+
     if(!this._isInit) {
-      this._setMineElems();
       this._isInit = !this._isInit;
+      this._initCellIndex = this._cellElemsArr.indexOf(target);
       this._cellElemsArr.forEach((item) => {
         item.removeEventListener('click', this._setInitActive);
       });
+      this._setElemsType();
     };
     console.log(`2: ${this._isInit}`);
   }
 
-  _setEventListeners(el) {
+  _initEvents(el) {
     el.addEventListener('click', this._setInitActive);
   }
 
@@ -84,8 +135,9 @@ export class Minecell {
         className: this._cellClassName,
         parentEl: this._wrapperEl
       });
+      cellEl.textContent = i.toString();
       this._cellElemsArr.push(cellEl);
-      this._setEventListeners(cellEl);
+      this._initEvents(cellEl);
       i++;
     }
   }
